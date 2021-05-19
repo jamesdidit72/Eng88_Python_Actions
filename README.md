f51423007399ba085d07c9d8278f166f53d4150b
jamesdidit72 <jamesjbarton72@gmail.com>
2021-05-19 16:18:02 +0100

    added tuple prints

--- README.md
+++ README.md
@@ -47,4 +47,8 @@ student_1 = {
 
 print(student_1['completed_lessons'])
 print(student_1['completed_lesson_names'])
+print(student_1['completed_lesson_names'][2])  # prints one index from the list
+print(student_1.keys())  # only prints the keys within the dictionary
+print(student_1.values())  # only prints the values within the dictionary
+
 ```
--- python_dictionaries.py
+++ python_dictionaries.py
@@ -12,5 +12,8 @@ student_1 = {
 
 }
 
-print(student_1['completed_lessons'])
-print(student_1['completed_lesson_names'])
+print(student_1)  # prints out the whole dictionary
+print(student_1['completed_lesson_names'])  # prints the value from the given key
+print(student_1['completed_lesson_names'][2])  # prints one index from the list
+print(student_1.keys())  # only prints the keys within the dictionary
+print(student_1.values())  # only prints the values within the dictionary
